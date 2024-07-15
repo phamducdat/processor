b6f201518c5bb6a18f727f1a231cce61df446bbd
phamducdat <phamducdat2402@gmail.com>
2024-07-15 09:00:32 +0700

    feat: Quest

--- src/main/java/com/wiinvent/gami/streams/entities/Quest.java
+++ src/main/java/com/wiinvent/gami/streams/entities/Quest.java
@@ -1,6 +1,9 @@
 package com.wiinvent.gami.streams.entities;
 
+import com.wiinvent.gami.avro.TaskState;
+import com.wiinvent.gami.streams.common.PeriodUnit;
 import lombok.Data;
+import org.apache.commons.lang3.ObjectUtils;
 
 @Data
 public class Quest {
@@ -11,6 +14,62 @@ public class Quest {
   private long taskId;
   private QuestType type;
   private int periodValue;
-  private P
+  private PeriodUnit periodUnit;
+  private CompletionRule completionRule;
+  private AggregateType aggregateType;
+
+
+  // TODO: I don't know
+  public static int callDynamicCount(TaskState taskState, PeriodUnit questPeriodUnit, int questPeriodValue) {
+    long startTime = taskState.getWindowStart();
+    long endTime = taskState.getWindowEnd();
+
+    return (int) ((endTime - startTime) / (questPeriodValue * questPeriodUnit.toMillis()));
+
+  }
+
+  public AggregateType getAggregateType() {
+    return ObjectUtils.defaultIfNull(aggregateType, AggregateType.SUM);
+  }
+
+  public enum AggregateType {
+    SUM,
+    LAST,
+  }
+
+
+  @Data
+  public static class CompletionRule {
+    private int minCount;
+    private int maxCount;
+    private boolean isContinuous;
+
+    public int getMinCount(TaskState taskState, PeriodUnit questPeriodUnit, int questPeriodValue) {
+      int count = minCount;
+      if (count == DYNAMIC_COUNT) {
+        count = Quest.callDynamicCount(taskState, questPeriodUnit, questPeriodValue);
+      }
+      return count;
+    }
+
+    public int getMaxCount(TaskState taskState, PeriodUnit questPeriodUnit, int questPeriodValue) {
+      int count = maxCount;
+      if (count == DYNAMIC_COUNT) {
+        count = Quest.callDynamicCount(taskState, questPeriodUnit, questPeriodValue);
+      } else if (count < 0) {
+        count = Integer.MAX_VALUE;
+      }
+
+      return count;
+    }
+
+    public int getMaxCount() {
+      if (maxCount < 0) {
+        return Integer.MAX_VALUE;
+      }
+
+      return maxCount;
+    }
+  }
 
 }
