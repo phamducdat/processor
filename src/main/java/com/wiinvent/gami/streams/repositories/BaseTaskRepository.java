0eb1150e3f82302b216abb123e412755560b88db
phamducdat <phamducdat2402@gmail.com>
2024-08-02 17:21:04 +0700

    feat: update

--- src/main/java/com/wiinvent/gami/streams/repositories/BaseTaskRepository.java
+++ src/main/java/com/wiinvent/gami/streams/repositories/BaseTaskRepository.java
@@ -1,13 +1,13 @@
 package com.wiinvent.gami.streams.repositories;
 
 import com.wiinvent.gami.avro.EventLog;
+import com.wiinvent.gami.streams.entities.Quest;
+import com.wiinvent.gami.streams.entities.QuestType;
 import com.wiinvent.gami.streams.entities.Task;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.ObjectUtils;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -24,9 +24,52 @@ public abstract class BaseTaskRepository implements ITaskRepository {
     onTasksLoaded(tasks);
   }
 
-  protected abstract void onTasksLoaded(List<Task> tasks);
-
   protected abstract List<Task> loadTasks();
+
+  protected void onTasksLoaded(List<Task> tasks) {
+    updateTasks(tasks);
+    updateTasksByEvent();
+    updateTasksById();
+  }
+
+  private void updateTasksById() {
+    Map<Long, Task> newTasksById = new HashMap<>();
+
+    tasks.forEach(
+        task -> {
+          newTasksById.put(task.getId(), task);
+        });
+
+    tasksById = newTasksById;
+  }
+
+  private void updateTasks(List<Task> taskList) {
+    Set<Task> newTasks = new HashSet<>(taskList);
+    newTasks.addAll(this.tasks);
+
+    this.tasks = newTasks;
+    log.info("{} task(s) loaded", this.tasks.size());
+  }
+
+  private void updateTasksByEvent() {
+    Map<String, List<Task>> newTasksByEvent = new HashMap<>();
+
+    tasks.forEach(
+        task -> {
+          List<Quest> quests = ObjectUtils.defaultIfNull(task.getQuests(), Collections.emptyList());
+          quests.forEach(
+              quest -> {
+                QuestType questType = quest.getType();
+                if (!newTasksByEvent.containsKey(questType.getExternalId())) {
+                  newTasksByEvent.put(questType.getExternalId(), new ArrayList<>());
+                }
+              }
+          );
+        }
+    );
+
+    tasksByEvent = newTasksByEvent;
+  }
 
   @Override
   public List<Task> findByEvent(EventLog eventLog) {
